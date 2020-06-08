26a7a2c0fbc3cc07eadc30c6f0351e26d56f4eb6
Gabriel <dreadpool38@gmail.com>
2020-06-08 17:02:07 -0300

    [ADD] Action

--- src/Redux/Reducer.jsx
+++ src/Redux/Reducer.jsx
@@ -1,12 +1,20 @@
-
+const ADD_QUESTIONS = 'ADD_QUESTIONS'
 
 const initialState = {
-
+    questions:[]
 }
 
 
 
-function reducer(store = initialState, action){
+function reducer(state = initialState, action){
+
+    switch (action.type) {
+        case ADD_QUESTIONS:
+            return {...state, questions:[...action.payload]}
+    
+        default:
+            break;
+    }
 
 }
 
