e59e37cf9eb6f2bbb15c6ccaec99b4eba602d631
Mateus Moreira <ashuraw@hotmail.com>
2020-06-09 17:34:05 -0300

    Tela de inicio 80%

---
+++ src/pages/TeladeFeedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TeladeFeedback extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TeladeFeedback
---
+++ src/pages/TeladeRanking.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TeladeRanking extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TeladeRanking
