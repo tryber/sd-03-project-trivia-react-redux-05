79c2e9512010a738b92c92c2e585346fe368d7c9
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-09 19:17:34 -0300

    codeclimate

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -19,14 +19,39 @@ const ranking = [
   },
 ];
 
+// thanks
+  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+const dynamicSort = (objKey) => {
+  let sortOrder = 1;
+  let property = objKey;
+  if (property[0] === '-') {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    let result = 0;
+    if (a[property] > b[property]) { 
+      result = - 1;
+    } 
+    else if (a[property] < b[property]) { 
+      result = 1; 
+    } else { 
+      result = 0;
+    }
+    return result * sortOrder;
+  };
+};
+
 class Ranking extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       goHome: false,
     };
     this.handleClick = this.handleClick.bind(this);
-    this.dynamicSort = this.dynamicSort.bind(this);
   }
 
   handleClick() {
@@ -35,26 +60,9 @@ class Ranking extends React.Component {
     });
   }
 
-  // thanks 
-  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
-  dynamicSort(property) {
-    let sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-      /* next line works with strings and numbers, 
-       * and you may want to customize it to your needs
-       */
-      let result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
-      return result * sortOrder;
-    }
-  }
-
   render() {
     const { goHome } = this.state;
-    const sortedRanking = ranking.sort(this.dynamicSort('score'));
+    const sortedRanking = ranking.sort(dynamicSort('score'));
     return (
       <div>
         {goHome && <Redirect to="/" />}
@@ -67,10 +75,10 @@ class Ranking extends React.Component {
         </div>
         <div>
           {sortedRanking.map((e, index) => <p key={e.name}>
-            <img src={e.picture} alt={`${e.name}`}/>
-              <span data-testid={`player-name-${index}`}>{e.name}</span>
-              <span data-testid={`player-score-${index}`}>{e.score}</span>
-            </p>)
+            <img src={e.picture} alt={`${e.name}`} />
+            <span data-testid={`player-name-${index}`}>{e.name}</span>
+            <span data-testid={`player-score-${index}`}>{e.score}</span>
+          </p>)
           }
         </div>
       </div>
