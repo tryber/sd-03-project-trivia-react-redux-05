043076cdcd5a5735d35f1aa794a5a141a43442ce
Mateus Moreira <ashuraw@hotmail.com>
2020-06-22 13:23:39 -0300

    deixando em 100%

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
